7ee6cc0bae343aa3e37e560f8d5b69e515a1cf71
Sylvester Henrique <henrique.sylvester@gmail.com>
2021-10-31 14:20:12 -0300

    Initial project configuration

---
+++ DIO.Movies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
